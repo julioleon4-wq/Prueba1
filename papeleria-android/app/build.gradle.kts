870a6c6465722f4f90d78f730acac1713883e3f9
Linkko <julio.leon.4@gmail.com>
2026-08-25 22:58:37 -0600

    Bump kiosk to v5

--- papeleria-android/app/build.gradle.kts
+++ papeleria-android/app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "mx.edu.uanl.papeleriaauto"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "4.0.0-kiosk-commercial"
+        versionCode = 5
+        versionName = "5.0.0-kiosk-printflow"
     }
     buildTypes {
         release { isMinifyEnabled = false }
